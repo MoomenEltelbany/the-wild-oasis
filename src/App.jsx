8b7549fc4823fddc424044bdc9fafc6fcc086610
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-09-19 19:26:19 +0300

    Add the main grid style to the AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,28 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="bookings" element={<Bookings />} />
-                <Route path="cabins" element={<Cabins />} />
-                <Route path="users" element={<Users />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="account" element={<Account />} />
-                <Route path="login" element={<Login />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route index element={<Dashboard />} />
+                        <Route path="bookings" element={<Bookings />} />
+                        <Route path="cabins" element={<Cabins />} />
+                        <Route path="users" element={<Users />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="account" element={<Account />} />
+                    </Route>
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
