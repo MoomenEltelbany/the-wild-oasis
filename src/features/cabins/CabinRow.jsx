6d219117397dc899bdff2d07f0c5bcf6b596d1d5
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-09-23 22:06:05 +0300

    Add the Cabin Row with data from Supabase & Install date-fns

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
     display: grid;
@@ -39,3 +39,19 @@ const Discount = styled.div`
     font-weight: 500;
     color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+    const { name, maxCapacity, regularPrice, discount, image } = cabin;
+    return (
+        <TableRow>
+            <Img src={image} alt={`Cabin ${name} image`} />
+            <Cabin>{name}</Cabin>
+            <div>Fits up to {maxCapacity} people</div>
+            <Price>{formatCurrency(regularPrice)}</Price>
+            <Discount>{formatCurrency(discount)}</Discount>
+            <button>Delete</button>
+        </TableRow>
+    );
+}
+
+export default CabinRow;
