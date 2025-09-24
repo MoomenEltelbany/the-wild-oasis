ed41f6830ecd02c8604d957676114a987c6ffcbb
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-09-24 21:56:05 +0300

    Install react-hot-toast and display toasts in CabinRow component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Login from "./pages/Login";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -27,6 +28,28 @@ function App() {
             <ReactQueryDevtools initialIsOpen={false} />
 
             <GlobalStyles />
+
+            <Toaster
+                position="top-center"
+                gutter={12}
+                containerStyle={{ margin: "8px" }}
+                toastOptions={{
+                    success: {
+                        duration: 3000,
+                    },
+                    error: {
+                        duration: 5000,
+                    },
+                    style: {
+                        fontSize: "16px",
+                        maxWidth: "500px",
+                        padding: "16px 24px",
+                        backgroundColor: "var(--color-grey-0)",
+                        color: "var(--color-grey-700)",
+                    },
+                }}
+            />
+
             <BrowserRouter>
                 <Routes>
                     <Route element={<AppLayout />}>
