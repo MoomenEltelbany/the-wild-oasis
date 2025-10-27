9a9a376a07ff5a4f204f5ae13d12fd1b82f39e83
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-10-27 20:30:40 +0300

    Add the functionality of closing the modal upon click outside it

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,8 @@
 import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
-import { HiXMark } from "react-icons/hi2";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 import styled from "styled-components";
+import { HiXMark } from "react-icons/hi2";
 
 const StyledModal = styled.div`
     position: fixed;
@@ -77,11 +78,13 @@ function Open({ children, opens: opensWindowName }) {
 function Window({ children, name }) {
     const { openName, close } = useContext(ModalContext);
 
+    const ref = useOutsideClick(close);
+
     if (name !== openName) return null;
 
     return createPortal(
         <Overlay>
-            <StyledModal>
+            <StyledModal ref={ref}>
                 <Button onClick={close}>
                     <HiXMark />
                 </Button>
